7bca36ff3d895805dfe14041836d7b877cd78db0
prabhalika <prabhalika999@gmail.com>
2015-02-19 13:01:55 +0000

    intial

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+  
+  def action
+  end
+  def contact
+  end
+  def about
+  end
+  
+end
---
+++ db/migrate/20150217070002_drop_static_pages.rb
@@ -0,0 +1,4 @@
+class DropStaticPages < ActiveRecord::Migration
+  def change
+  end
+end
