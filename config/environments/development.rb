6fe5d9bc485a4589b6e665a091016df4252bc6b2
PRABHALIKA <Prabhalika999@gmail.com>
2015-02-27 18:44:12 +0530

    intial

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -25,6 +25,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+
   config.assets.debug = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
@@ -38,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host}
 end
